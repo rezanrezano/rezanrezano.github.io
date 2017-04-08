bc3085f18579c5e97d271aab3f271ebb1d8a537d
rezanrezano <captenrez@gmail.com>
2017-04-08 22:20:11 +0300

    finish to add information to  projects file

--- js/projects.js
+++ js/projects.js
@@ -9,7 +9,7 @@ function makeAjaxRequest(){
 			
 			console.log(repo);
 
-			html += (`<li><div class='box'><a href= '${repo.html_url}' target	="_blank">${repo.name}</a> <p>Language: ${repo.language}</p> Updated at: ${repo.updated_at}</div></li>`);
+			html += (`<li><div class='box'><a href= '${repo.html_url}' target	="_blank">${repo.name}</a> <p>Language: ${repo.language}</p> Updated at: ${repo.updated_at} </div></li>`);
 		})
 
 		html += "</ul>"
